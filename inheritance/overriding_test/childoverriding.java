6126071510fcdbb5b62a0673573b99fac104fe69
leenuu <61344683+leenuu@users.noreply.github.com>
2021-08-26 15:39:05 +0900

    Create childoverriding.java

---
+++ inheritance/overriding_test/childoverriding.java
@@ -0,0 +1,14 @@
+package inheritance.overriding_test;
+
+public class childoverriding extends parentoverriding
+{
+	public childoverriding()
+	{
+		System.out.println("childoverriding");
+	}
+	
+	public void printname()
+	{
+		System.out.println("child");
+	}
+}
