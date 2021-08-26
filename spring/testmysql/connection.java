940941fcbdceb72c7ba4241ce9f9a44487578a52
leenuu <61344683+leenuu@users.noreply.github.com>
2021-08-26 12:24:08 +0900

    Create connection.java

---
+++ spring/testmysql/connection.java
@@ -0,0 +1,48 @@
+package testmysql;
+import java.sql.*;
+
+public class connection 
+{
+	public static void main(String[] args)
+	{
+		Connection con = null;
+		
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			String url = "jdbc:mysql://ip";
+			
+			con = DriverManager.getConnection(url, "user", "password");
+			System.out.println("연결 성공");
+		}
+		
+		catch(ClassNotFoundException e)
+		{
+			System.out.println("드라이버 로딩 실패");
+	    }
+		
+	    catch(SQLException e)
+		{
+	    	System.out.println("에러: " + e);
+	    }
+		
+	    finally
+	    {
+	    	try
+	    	{
+	    		if( con != null && !con.isClosed())
+	    		{
+	    			con.close();
+	            }
+	         }
+	    	
+	         catch( SQLException e)
+	    	{
+	        	 e.printStackTrace();
+	        }
+	    }
+		
+		System.out.println("test finish");
+	}
+}
