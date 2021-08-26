eb03bf9c7c7793c5cbdd884dcccb3e09395de039
leenuu <61344683+leenuu@users.noreply.github.com>
2021-08-26 15:39:25 +0900

    Add files via upload

---
+++ inheritance/overriding_test/parentoverriding.java
@@ -0,0 +1,14 @@
+package inheritance.overriding_test;
+
+public class parentoverriding 
+{
+	public parentoverriding()
+	{
+		System.out.println("parentoverridng");
+	}
+	
+	public void printname()
+	{
+		System.out.println("parent");
+	}
+}
---
+++ inheritance/overriding_test/testprint.java
@@ -0,0 +1,10 @@
+package inheritance.overriding_test;
+
+public class testprint
+{
+	public static void main(String[] args)
+	{
+		childoverriding test = new childoverriding();
+		test.printname();
+	}
+}
